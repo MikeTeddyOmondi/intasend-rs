bcc93c6096f13df1d6a32208a337528d3ae268e9
MikeTeddyOmondi <mike_omondi@outlook.com>
2024-02-21 23:36:20 +0300

    Updates: added crate docs to `checkout` module and refactored code

--- intasend/src/intasend/checkout.rs
+++ intasend/src/intasend/checkout.rs
@@ -11,12 +11,48 @@ use crate::Intasend;
 
 use super::{Currency, FromJsonValue, RequestClient, RequestMethods};
 
+/// Checkout struct implements methods for facilitating:
+/// Checkout Link API that allows you to generate a secure link that you can
+/// share with your customer to complete payment. Once the customer click on the link,
+/// IntaSend provides a guide on how to complete payment and notify both you and the customers on the status of the payment.
+///
+/// ```rust
+/// // Intasend client
+/// let intasend = Intasend::new(
+///     publishable_key: "ISPubKey_test_c1f90113-3dbb-4201-9b88-f1c2d3056e5c".to_string(),
+///     secret_key: "ISSecretKey_test_5527b085-40b6-460a-9c31-25d58a204d20".to_string(),
+///     test_mode: true
+/// );
+///
+/// // Collection API
+/// let checkout: Checkout = intasend.checkout();
+/// ```
+///
 #[derive(Debug)]
 pub struct Checkout {
     pub(crate) intasend: Intasend,
 }
 
 impl Checkout {
+    /// The initiate method generates a checkout link that you'll send to your customers to complete payment
+    ///
+    /// ```rust
+    /// // Checkout API
+    /// let checkout: Checkout = intasend.checkout();
+    ///
+    /// let checkout_req = CheckoutRequest {
+    ///     first_name: Some("Joe".to_string()),
+    ///     last_name: Some("Doe".to_string()),
+    ///     email: Some("joe@doe.com".to_string()),
+    ///     method: Some(CheckoutMethod::CARDPAYMENT.as_str()),
+    ///     amount: Decimal::new(10, 2),
+    ///     currency: Currency::USD,
+    /// };
+    ///
+    /// let checkout_response: CheckoutResponse = checkout.initiate(checkout_req).await?;
+    /// println!("Checkout response: {:#?}", checkout_response);
+    /// ```
+    ///
     pub async fn initiate(&self, payload: CheckoutRequest) -> Result<CheckoutResponse, Error> {
         // let service_path: &str = "/api/v1/checkout/";
         // let request_method: RequestMethods = RequestMethods::POST;
@@ -45,7 +81,7 @@ impl Checkout {
         };
 
         let response = client
-            .post(&format!("{}/api/v1/payment/status/", base_url))
+            .post(&format!("{}/api/v1/checkout/", base_url))
             .header("Content-Type", "application/json")
             // .header(
             //     "Authorization",
@@ -61,15 +97,31 @@ impl Checkout {
 
         let json_response =
             serde_json::from_value::<JSON>(response?.json().await?).expect("Error parsing json!");
+        println!("Response: {:#?}", json_response);
 
-        let status_response = CheckoutResponse::from_value(&json_response).unwrap();
-        println!("Response: {:#?}", status_response);
+        let checkout_response = CheckoutResponse::from_value(&json_response).unwrap();
+        println!("Response: {:#?}", checkout_response);
 
         // let status_response: CheckoutResponse = response?.json().await?;
 
-        Ok(status_response)
+        Ok(checkout_response)
     }
 
+    /// The details method approves send money request.
+    ///
+    /// ```rust
+    /// // Checkout API
+    /// let checkout: Checkout = intasend.checkout();
+    ///
+    /// let checkout_details_req = CheckoutDetailsRequest {
+    ///     checkout_id: "dd4bd8a2-a34c-4c04-9663-6935eb8a8a4b".to_string(),
+    ///     signature: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzY29wZSI6ImV4cHJlc3MtY2hlY2tvdXQiLCJpc3MiOiJJbnRhU2VuZCBTb2x1dGlvbnMgTGltaXRlZCIsImF1ZCI6WyJkZDRiZDhhMi1hMzRjLTRjMDQtOTY2My02OTM1ZWI4YThhNGIiXSwiaWF0IjoxNzA4NTQ1ODgyLCJleHAiOjE3MDg1NDk0ODIsImFjY291bnRJRCI6IjlRSjlLR1kiLCJyZWZlcmVuY2UiOiJkZDRiZDhhMi1hMzRjLTRjMDQtOTY2My02OTM1ZWI4YThhNGIifQ.lnHBsBqzAaM24UxdL82nrGIlpDKBlGG-tCJDocMkrZk".to_string(),
+    /// };
+    ///
+    /// let checkout_details_response: CheckoutResponse = checkout.details(checkout_details_req).await?;
+    /// println!("Checkout details response: {:#?}", checkout_details_response);
+    /// ```
+    ///
     pub async fn details(
         &self,
         payload: CheckoutDetailsRequest,
@@ -83,7 +135,7 @@ impl Checkout {
         };
 
         let response = client
-            .post(&format!("{}/api/v1/payment/status/", base_url))
+            .post(&format!("{}/api/v1/checkout/details/", base_url))
             .header("Content-Type", "application/json")
             .header(
                 "Authorization",
@@ -108,7 +160,7 @@ pub struct CheckoutRequest {
     pub first_name: Option<String>,
     pub last_name: Option<String>,
     pub email: Option<String>,
-    pub method: Option<CheckoutMethod>,
+    pub method: Option<String>,
     pub amount: Decimal,
     pub currency: Currency,
 }
@@ -180,12 +232,8 @@ impl FromJsonValue for CheckoutResponse {
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct CheckoutDetailsRequest {
-    pub invoice: Option<String>,
-    pub customer: Option<String>,
-    pub payment_link: Option<String>,
-    pub refundable: bool,
-    pub created_at: String,
-    pub updated_at: String,
+    pub checkout_id: String,
+    pub signature: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -208,7 +256,7 @@ pub enum CheckoutMethod {
 }
 
 impl CheckoutMethod {
-    fn as_str(&self) -> String {
+    pub fn as_str(&self) -> String {
         match self {
             CheckoutMethod::MPESA => "M-PESA".to_string(),
             CheckoutMethod::CARDPAYMENT => "CARD-PAYMENT".to_string(),
@@ -219,6 +267,7 @@ impl CheckoutMethod {
     }
 }
 
+// Checkout Response
 // {
 //   "id": "0d035afb-8a8b-49ba-ac75-e3c756d8528b",
 //   "url": "https://sandbox.intasend.com/checkout/0d035afb-8a8b-49ba-ac75-e3c756d8528b/express/",
@@ -258,3 +307,44 @@ impl CheckoutMethod {
 //   "last_name": null,
 //   "phone_number": null,
 //   "email": null,
+
+// Checkout Details Response
+// {
+//   "id": "dd4bd8a2-a34c-4c04-9663-6935eb8a8a4b",
+//   "url": "https://sandbox.intasend.com/checkout/dd4bd8a2-a34c-4c04-9663-6935eb8a8a4b/express/",
+//   "signature": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzY29wZSI6ImV4cHJlc3MtY2hlY2tvdXQiLCJpc3MiOiJJbnRhU2VuZCBTb2x1dGlvbnMgTGltaXRlZCIsImF1ZCI6WyJkZDRiZDhhMi1hMzRjLTRjMDQtOTY2My02OTM1ZWI4YThhNGIiXSwiaWF0IjoxNzA4NTQ2MTI4LCJleHAiOjE3MDg1NDk3MjgsImFjY291bnRJRCI6IjlRSjlLR1kiLCJyZWZlcmVuY2UiOiJkZDRiZDhhMi1hMzRjLTRjMDQtOTY2My02OTM1ZWI4YThhNGIifQ.Pa6EwuVJhV5LtLU_NkIprarxkHgoywjlV_CK7L68uj4",
+//   "first_name": null,
+//   "last_name": null,
+//   "phone_number": null,
+//   "email": null,
+//   "country": null,
+//   "address": null,
+//   "city": null,
+//   "state": null,
+//   "zipcode": null,
+//   "api_ref": null,
+//   "wallet_id": null,
+//   "method": null,
+//   "channel": "WEBSITE",
+//   "host": "127.0.0.1",
+//   "is_mobile": false,
+//   "version": null,
+//   "redirect_url": null,
+//   "amount": "0.00",
+//   "currency": "KES",
+//   "paid": false,
+//   "mobile_tarrif": "BUSINESS-PAYS",
+//   "card_tarrif": "BUSINESS-PAYS",
+//   "bitcoin_tarrif": "BUSINESS-PAYS",
+//   "ach_tarrif": "BUSINESS-PAYS",
+//   "created_at": "2024-02-21T23:04:42.566094+03:00",
+//   "updated_at": "2024-02-21T23:04:42.566121+03:00",
+//   "defaults": {
+//     "enable_card_payment": true,
+//     "enable_mpesa_payment": true,
+//     "enable_bitcoin_payment": false,
+//     "enable_ach_payment": false,
+//     "default_currency": "USD",
+//     "default_tarrif": "BUSINESS-PAYS"
+//   }
+// }
