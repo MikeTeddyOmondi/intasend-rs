2ab1431790f2ee27b20c78149d5dc839feda83dd
MikeTeddyOmondi <mike_omondi@outlook.com>
2025-04-11 00:43:59 +0300

    Updates: edited feature flags; & added `payment_link` module

--- intasend/src/lib.rs
+++ intasend/src/lib.rs
@@ -2,30 +2,34 @@
 
 mod intasend;
 
-#[cfg(feature = "client")]
-#[cfg(feature = "server")]
+// Checkout functionality - available for both `client` and `server` environments
+#[cfg(any(feature = "client", feature = "server"))]
 pub use self::intasend::checkout::{
     CheckoutDetailsRequest, CheckoutDetailsResponse, CheckoutMethod, CheckoutRequest,
     CheckoutResponse, CheckoutsAPI,
 };
 
+// Collection functionality - `server` only
 #[cfg(feature = "server")]
 pub use self::intasend::collection::{
     CollectionsAPI, MpesaStkPushRequest, MpesaStkPushResponse, StkPushStatusRequest,
     StkPushStatusResponse,
 };
 
+// Payout functionality - `server` only
 #[cfg(feature = "server")]
 pub use self::intasend::payouts::{
     Payout, PayoutApproval, PayoutApprovalRequest, PayoutRequest, PayoutRequestTransaction,
     PayoutResponseTransaction, PayoutStatusRequest, PayoutsAPI,
 };
 
+// Refund functionality - `server` only
 #[cfg(feature = "server")]
 pub use self::intasend::refunds::{
     Refund, RefundListResponse, RefundReason, RefundRequest, RefundsAPI,
 };
 
+// Wallet functionality - `server` only
 #[cfg(feature = "server")]
 pub use self::intasend::wallets::{
     FundCheckoutRequest, FundCheckoutResponse, FundMpesaRequest, FundMpesaResponse, Wallet,
@@ -34,8 +38,15 @@ pub use self::intasend::wallets::{
     WalletsAPI,
 };
 
-#[cfg(feature = "client")]
+// Payment Links functionality - `server` only
 #[cfg(feature = "server")]
+pub use self::intasend::payment_links::{
+    PaymentLinksAPI, PaymentLinksCreateDetails, PaymentLinksDetailsRequest,
+    PaymentLinksListRequest, PaymentLinksListResponse,
+};
+
+// Core types - available for both `client` and `server` environments
+#[cfg(any(feature = "client", feature = "server"))]
 pub use self::intasend::{
     Currency, Intasend, IntasendApiError, IntasendApiErrorDetail, IntasendClientError,
     PayoutProvider, Provider, Tarrif, Transaction, TransactionStatus, TransactionType,
