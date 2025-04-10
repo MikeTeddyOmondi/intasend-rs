15121f67501abcfaf06f46a4c48b789671f0f008
MikeTeddyOmondi <mike_omondi@outlook.com>
2025-04-11 01:29:53 +0300

    Updates: edited `payment_links` example

--- examples/src/bin/payment_links.rs
+++ examples/src/bin/payment_links.rs
@@ -28,20 +28,20 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let payment_links = intasend.payment_links();
     println!("[#] PaymentLinksAPI instance: {:#?}", payment_links);
 
-    // let payment_links_list = payment_links.list().await?;
-    // println!("[#] Payment Links List: {:#?}", &payment_links_list);
+    let payment_links_list = payment_links.list().await?;
+    println!("[#] Payment Links List: {:#?}", &payment_links_list);
 
-    // let payment_link_id = Uuid::parse_str("5284042b-f13d-49c2-abdf-793fbaf9b8e2").unwrap(); // PaymentLink ID: 5284042b-f13d-49c2-abdf-793fbaf9b8e2
-    // let payment_link_details = payment_links.details(payment_link_id.to_string()).await?;
-    // println!("[#] Payment Link Details: {:#?}", payment_link_details);
+    let payment_link_id = Uuid::parse_str("5284042b-f13d-49c2-abdf-793fbaf9b8e2").unwrap(); // PaymentLink ID: 5284042b-f13d-49c2-abdf-793fbaf9b8e2
+    let payment_link_details = payment_links.details(payment_link_id.to_string()).await?;
+    println!("[#] Payment Link Details: {:#?}", payment_link_details);
 
-    // // Create a Wallet
-    // let wallet_label_identifier = "wallet-label".to_string();
-    // let label = format!("{}-{}", wallet_label_identifier, Uuid::new_v4());
+    // Create a payment link
+    let payment_link_label_identifier = "payment-link-label".to_string();
+    let payment_link_label = format!("{}-{}", payment_link_label_identifier, Uuid::new_v4());
 
     let payload = PaymentLinksCreateDetails {
         currency:Currency::Kes, 
-        title: format!("{}-{}", String::from("PaymentLink"), Uuid::new_v4()), 
+        title: payment_link_label, 
         amount: Some(100), 
         usage_limit: Some(3), 
         is_active: Some(true), 
@@ -54,61 +54,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let created_payment_link = payment_links.create(payload).await?;
     println!("[#] Payment Link Created: {:#?}", created_payment_link);
 
-    // // Get transactions from a Wallet
-    // let wallet_transanctions = wallets_api.transactions(wallet_id.clone()).await?;
-    // println!("[#] Wallet Transactions: {:#?}", wallet_transanctions);
-
-    // let source_wallet_id = wallet_id.clone(); // "VRELLZQ".to_string();
-    // let intra_transfer_payload = WalletIntraTransferRequest {
-    //     wallet_id: "VRELLZQ".to_string(), // "Y7ELXJQ".to_string();
-    //     amount: Decimal::new(10000, 2),
-    //     narrative: "wallet funding".to_string(),
-    // };
-    // let wallets_transfer_response = wallets_api
-    //     .intra_transfer(source_wallet_id, intra_transfer_payload)
-    //     .await?;
-    // println!(
-    //     "[#] Wallet Transfer Response: {:#?}",
-    //     wallets_transfer_response
-    // );
-
-    // let fund_mpesa_payload = FundMpesaRequest {
-    //     method: Provider::Mpesa,
-    //     currency: Currency::Kes,
-    //     amount: Decimal::new(1000, 2),
-    //     wallet_id: "Y7ELXJQ".to_string(),
-    //     phone_number: "254717135176".to_string(),
-    // };
-
-    // let fund_mpesa_response = wallets_api.fund_mpesa(fund_mpesa_payload).await?;
-    // println!("[#] Wallet Fund Mpesa Response: {:#?}", fund_mpesa_response);
-    // tokio::time::sleep(std::time::Duration::from_secs(30)).await;
-
-    // let wallet_details = wallets_api.details("Y7ELXJQ".to_string()).await?;
-    // println!("[#] Wallet Details: {:#?}", wallet_details);
-
-    // let fund_checkout_req = FundCheckoutRequest {
-    //     first_name: Some("Foo".to_string()),
-    //     last_name: Some("Bar".to_string()),
-    //     email: Some("foobar@baz.com".to_string()),
-    //     method: Some(Provider::Bank),
-    //     amount: Decimal::new(100000, 2), // 1000.00
-    //     currency: Currency::Kes,
-    //     wallet_id: "Y7ELXJQ".to_string(),
-    //     api_ref: None,
-    //     redirect_url: None,
-    // };
-    // println!("[#] Fund Checkout Request: {:#?}", fund_checkout_req);
-    // println!(
-    //     "[#] Fund Checkout request (JSON): {:#?}",
-    //     serde_json::to_value(&fund_checkout_req)
-    // );
-
-    // let fund_checkout_response: FundCheckoutResponse =
-    //     wallets_api.fund_checkout(fund_checkout_req).await?;
-    // println!("[#] Fund Checkout response: {:#?}", fund_checkout_response);
-
-    // println!("Lib works!");
+    println!("Lib works!");
 
     Ok(())
 }
